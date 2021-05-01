ec9930459a01392bb64d305e4ab1336b2e1cda0f
kalebetucker <kalebetucker@gmail.com>
2021-05-01 18:25:55 -0400

    updates for new trees.

--- Group Project Script CM.R
+++ Group Project Script CM.R
@@ -154,8 +154,15 @@ logistic_predictions$validation_predictions <- as.integer(logistic_predictions$v
 
 # Validation Accuracy
 mean(~(outcome_type == validation_predictions), data = logistic_predictions)
-# Validation Accuracy - X%
+# Validation Accuracy - 83.54978%
 
+# Test Predictions
+logistic_predictions <- mutate(asdf1_test, test_predictions = predict(logistic_model, asdf1_test, type="response") %>% round())
+logistic_predictions$test_predictions <- as.integer(logistic_predictions$test_predictions)
+
+# Test Accuracy
+mean(~(outcome_type == test_predictions), data = logistic_predictions)
+# Test Accuracy - 78.01%
 
 # Logistic Regression - KFold
 # Model - 10 Folds 
@@ -167,7 +174,8 @@ logistic_kfold
 # Model Predictions & Accuracy 
 asdf1_logistic_kfold_predictions <- mutate(asdf1_kfold_test, model_predictions = predict(logistic_kfold, newdata = asdf1_kfold_test))
 mean(~(outcome_type == model_predictions), data = asdf1_logistic_kfold_predictions )
-# Model Accuracy X%
+# Model Accuracy 81.3%
+
 
 
 #--------------------------------------------------------------------------#
@@ -181,6 +189,16 @@ summary(stepmodel)
 stepwise_validation  <- mutate(asdf1_valid, validation_predictions = round(predict(stepmodel, newdata = asdf1_valid, type = "response"),0))
 mean(~(outcome_type == validation_predictions), data = stepwise_validation)
 # Validation Data Accuracy - X%
+
+# Test Predictions
+stepwise_predictions <- mutate(asdf1_test, test_predictions = predict(stepmodel, asdf1_test, type="response") %>% round())
+stepwise_predictions$test_predictions <- as.integer(stepwise_predictions$test_predictions)
+
+# Test Accuracy
+mean(~(outcome_type == test_predictions), data = stepwise_predictions)
+# Test Accuracy - 70.92%
+
+
 
 
 # Stepwise Regression - KFold
@@ -210,7 +228,13 @@ rpart.plot(tree_model_prun_01,roundint=FALSE,nn=TRUE,extra=4)
 # Validation Predictions & Accuracy
 tree_prun_01_validate  <- mutate(asdf1_valid, validation_predictions = predict(tree_model_prun_01, newdata = asdf1_valid, type = "class"))
 mean(~(outcome_type == validation_predictions), data = tree_prun_01_validate)
-# Validation Accuracy X%
+# Validation Accuracy 83.8%
+
+# Test Predictions & Accuracy
+tree_prun_01_test  <- mutate(asdf1_test, test_predictions = predict(tree_model_prun_01, newdata = asdf1_test, type = "class"))
+mean(~(outcome_type == test_predictions), data = tree_prun_01_test)
+# Validation Accuracy 84.21%
+
 
 # CP - .03
 tree_model_prun_03 <- prune(tree_model, cp = 0.03)
@@ -219,13 +243,55 @@ rpart.plot(tree_model_prun_03,roundint=FALSE,nn=TRUE,extra=4)
 # Validation Predictions & Accuracy 
 tree_prun_03_validate  <- mutate(asdf1_valid, validation_predictions = predict(tree_model_prun_03, newdata = asdf1_valid, type = "class"))
 mean(~(outcome_type == validation_predictions), data = tree_prun_03_validate)
-# Validation Accuracy X%
+# Validation Accuracy 83.8%
+
+# Test Predictions & Accuracy 
+tree_prun_03_test  <- mutate(asdf1_test, test_predictions = predict(tree_model_prun_03, newdata = asdf1_test, type = "class"))
+mean(~(outcome_type == test_predictions), data = tree_prun_03_test)
+# Validation Accuracy 82.7%
+
+
+# CP - .05
+tree_model_prun_05 <- prune(tree_model, cp = 0.05)
+rpart.plot(tree_model_prun_05 ,roundint=FALSE,nn=TRUE,extra=4)
+
+# Validation Predictions & Accuracy 
+tree_prun_05_validate  <- mutate(asdf1_valid, validation_predictions = predict(tree_model_prun_05, newdata = asdf1_valid, type = "class"))
+mean(~(outcome_type == validation_predictions), data = tree_prun_05_validate)
+# Validation Accuracy 72.8
+
+# Test Predictions & Accuracy 
+tree_prun_05_test  <- mutate(asdf1_test, test_predictions = predict(tree_model_prun_05, newdata = asdf1_test, type = "class"))
+mean(~(outcome_type == test_predictions), data = tree_prun_05_test)
+# Validation Accuracy 74.4%
+
+
+# CP - .07
+tree_model_prun_07 <- prune(tree_model, cp = 0.07)
+rpart.plot(tree_model_prun_07,roundint=FALSE,nn=TRUE,extra=4)
+
+# Validation Predictions & Accuracy 
+tree_prun_07_validate  <- mutate(asdf1_valid, validation_predictions = predict(tree_model_prun_07, newdata = asdf1_valid, type = "class"))
+mean(~(outcome_type == validation_predictions), data = tree_prun_07_validate)
+# Validation Accuracy 74.4%
+
+# Test Predictions & Accuracy 
+tree_prun_07_test  <- mutate(asdf1_test, test_predictions = predict(tree_model_prun_07, newdata = asdf1_test, type = "class"))
+mean(~(outcome_type == test_predictions), data = tree_prun_07_test)
+# Validation Accuracy 82.7%
+
+
+
+
+
+
+
 
 
 # Decision Tree - KFold    
 
-# Model - 8 Complexity Parameters, 10 Folds 
-tree <- train(outcome_type ~ . , data = asdf1_kfold_train ,method = "rpart", trControl = trainControl("cv", number = 10 ), tuneLength = 8)     
+# Model - 10 Complexity Parameters, 10 Folds 
+tree <- train(outcome_type ~ . , data = asdf1_kfold_train ,method = "rpart", trControl = trainControl("cv", number = 10 ), tuneLength = 10)     
 
 # Model Summary
 tree
@@ -234,7 +300,7 @@ plot(tree)
 # Best CP = 0.001798561
 
 # Model w/ Recommended CP
-tree_model_kfold <- rpart(outcome_type ~ ., data = asdf1_kfold_train, method="class" , cp = 0.003669725)
+tree_model_kfold <- rpart(outcome_type ~ ., data = asdf1_kfold_train, method="class" , cp = 0.005338078)
 rpart.plot(tree_model_kfold, roundint = FALSE, nn = TRUE, extra = 1)
 rpart.plot(tree_model_kfold, roundint = FALSE, nn = TRUE, extra = 4)
 
@@ -265,6 +331,19 @@ nn_predictions <- mutate(asdf1_norm_valid, validation_predictions = predict(nn1,
 mean(~(outcome_type == validation_predictions),data = nn_predictions) 
 # Validation Accuracy - X%
 
+
+# Validation Predictions
+nn_predictions <- mutate(asdf1_norm_test, test_predictions = predict(nn1,asdf1_norm_test) %>% round())
+
+# Validation Accuracy
+mean(~(outcome_type == test_predictions),data = nn_predictions) 
+# Validation Accuracy - 78.01%
+
+
+
+
+
+
 # NNet - KFold
 # Model - 10 Folds 
 nnet_kfold <- train(outcome_type ~ . , data = asdf1_norm_kfold_train ,method = 'avNNet', trControl = trainControl("cv", number = 1 ))     
@@ -291,7 +370,17 @@ svm_validation_predictions <- mutate(svm_validation_predictions, validation_pred
 
 # Validation Accuracy
 mean(~(outcome_type == validation_predictions), data = svm_validation_predictions)
-# Validation Accuracy - X%%
+# Validation Accuracy - 82.5%%
+
+
+# Test Predictions
+svm_test_predictions <- predict(svmfit,asdf1_norm_test,probability=TRUE)
+svm_test_predictions <- mutate(asdf1_norm_test, test_predictions = attributes(svm_test_predictions)$probabilities[,1]) #DF with Predictions
+svm_test_predictions <- mutate(svm_test_predictions, test_predictions = round(test_predictions)) # Rounding
+
+# Test Accuracy
+mean(~(outcome_type == test_predictions), data = svm_test_predictions)
+# Validation Accuracy - 81.1%%
 
 
 # SVM - KFold 
